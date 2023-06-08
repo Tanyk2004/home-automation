c40731b02236c73462c1648852f6a0f39f9dd040
Tanay Garg <tanay.garg@utexas.edu>
2023-06-08 22:17:53 +0530

    created a base app for both front end and backend

---
+++ frontend-service/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to my home control
+      <button style={{
+        width: '100px',
+        padding: '10px',
+        margin: '5em',
+      }}>Test</button>
+    </div>
+  );
+}
+
+export default App;
