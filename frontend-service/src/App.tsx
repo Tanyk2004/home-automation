75e667d20e8d0048597a309d04ba49ed714b034d
Tanay Garg <tanay.garg@utexas.edu>
2023-06-10 05:03:29 -0500

    made some changes?

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 function App() {
+
+
+  // TODO make card component, and a button component
+  
   return (
     <div className="App">
+
       Welcome to my home control
       <button style={{
         width: '100px',
