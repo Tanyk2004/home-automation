1ec07e244a8464ad26700404a00be661aff0104f
Tanay Garg <tanay.garg@utexas.edu>
2023-06-12 04:54:10 +0530

    connected front end and backend

--- backend-service/core/views.py
+++ backend-service/core/views.py
@@ -1,6 +1,20 @@
+from flask import jsonify, request
 from core import app
 import json
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return json.loads('{"message": "Hello World!"}')
+    return jsonify({"message": "Home Automation API is Running!"}), 200
+
+
+# TODO - create a get based api endpoint for getting the relay state
+
+# we want to use this to update the relay state
+# TODO - Now connect this to the GPIO code in a different class based on relay ID's
+@app.route('/relay', methods=['PUT'])
+def relay():
+    data = request.get_json()
+    headers= request.headers
+    print(data)
+    print(headers)
+    return jsonify({"updatedRelayState" : (data["relayState"] + 1)}), 200
--- frontend-service/src/pages/Homepage.tsx
+++ frontend-service/src/pages/Homepage.tsx
@@ -3,11 +3,27 @@ import BaseCard from '../components/BaseCard'
 import '../styles/homepage.css'
 import { useState, useEffect } from 'react'
 function Homepage() {
-  
+  const backendURL = 'http://localhost:5000'
   useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then( res => res.json())
-      .then( data => console.log(data["message"]))
+    console.log(backendURL)
+    // fetch(`${backendURL}/`)
+    //   .then( res => res.json())
+    //   .then( data => console.log(data["message"]))
+
+
+    fetch( `${backendURL}/relay` , {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        relayNumber: '1'
+      },
+      body: JSON.stringify({
+        relayNumber: '10',
+        relayState: 0
+      })
+    })
+    .then ( res => res.json())
+    .then ( data => console.log(data["updatedRelayState"]))
   }, [])
 
   return (
